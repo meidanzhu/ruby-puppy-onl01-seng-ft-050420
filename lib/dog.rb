6f4a5104d98ca03852dad4e2b88d0155b0c44417
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-14 15:45:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,20 @@
 class Dog
-
   attr_accessor :name
 
   @@all = []
 
   def initialize(name)
-      @name=name
-      @@all << self
-    end
+    @name = name
+    @@all << self
+  end
 
-    def self.clear_all
-      @@all.clear
-    end
+  def self.clear_all
+    @@all.clear
+  end
 
-    def self.all
-      @@all.each {|dog| puts "#{dog.name}"}
+  def self.all
+    @@all.each do |d|
+      puts d.name
     end
   end
+end
